d12932a53713a1d8302daea6571a2284946461ba
11372006 <594angel91729@gmail.com>
2025-02-25 20:16:11 +0800

    Create Exercise1.9.4.py

---
+++ 20250225/Exercise1.9.4.py
@@ -0,0 +1,20 @@
+a=type(765)
+print(a)
+
+b=type(2.718)
+print(b)
+
+c=type('2 pi')
+print(c)
+
+d=type(abs(-7))
+print(d)
+
+e=type(abs(-7.0))
+print(e)
+
+f=type(abs)
+print(f)
+#abs取絕對值
+#int整數.浮點數
+#type判斷形式
