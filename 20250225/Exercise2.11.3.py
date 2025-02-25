d6ce56de2a69595f8fb6c8128b91127757d24d2b
11372006 <594angel91729@gmail.com>
2025-02-25 20:16:17 +0800

    Create Exercise2.11.3.py

---
+++ 20250225/Exercise2.11.3.py
@@ -0,0 +1,22 @@
+import math
+#part 1
+r=5
+volume = math.pow(5,3)* math.pi *4 /3
+print ('part1 volume=', volume)
+
+#part 2
+x=42
+v= math.sin(x)**2+math.cos(x)**2
+
+print('part2=',v)
+
+#part 3
+volume = math.e**2
+print ('part3 volume=', volume)
+
+volume=math.pow(math.e, 2)
+print ('part4 volume=', volume)
+
+x=2
+volume=math.exp(x)
+print ('part5 volume=', volume)
