61fc5d6aa6372fa6acb46f557cd566c1d481923d
11372006 <594angel91729@gmail.com>
2025-03-04 20:19:09 +0800

    Create hw6.1.py

---
+++ 20250304/hw6.1.py
@@ -0,0 +1,10 @@
+def absolute_value(x):
+    if x < 0:
+        return -x
+    else:
+        return x
+
+# 測試範例
+print(absolute_value(-7))  # 輸出: 7
+print(absolute_value(5))   # 輸出: 5
+print(absolute_value(0))   # 輸出: 0
