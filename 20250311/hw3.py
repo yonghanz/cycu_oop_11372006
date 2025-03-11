8a36cf0f57097ebbc64252ae4b2832378e2e6df4
11372006 <594angel91729@gmail.com>
2025-03-11 20:37:01 +0800

    Create hw3.py

---
+++ 20250311/hw3.py
@@ -0,0 +1,11 @@
+# create a list fill with weekdays from Monday to Sunday.
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+# reate a list fill with months from January to December.
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+
+#get user input for the month
+month = input('Eter the month): ')
+#print the English name of the month form the list
+print(months[int(month)-1])
